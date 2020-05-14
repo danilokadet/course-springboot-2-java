eb2032d06d24de80377b8a4bf845faf751ccdd63
danilos <daniloks2@gmail.com>
2020-05-14 19:32:34 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -77,6 +77,11 @@ public class OrderItem implements Serializable{
 		return result;
 	}
 
+	public Double getSubTotal() {
+		
+		return price * quantity;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
